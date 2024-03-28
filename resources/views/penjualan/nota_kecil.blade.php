7f8ab5872ca8c23dcd7ebf9f01f52659977f46b2
fdaffa12 <faizdaffa17@gmail.com>
2024-03-28 07:40:51 +0700

    nota

--- resources/views/penjualan/nota_kecil.blade.php
+++ resources/views/penjualan/nota_kecil.blade.php
@@ -6,56 +6,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nota Kecil</title>
 
-    <?php
-    $style = '
     <style>
-        * {
-            font-family: "consolas", sans-serif;
-        }
-        p {
-            display: block;
-            margin: 3px;
-            font-size: 10pt;
-        }
-        table td {
-            font-size: 9pt;
-        }
-        .text-center {
-            text-align: center;
-        }
-        .text-right {
-            text-align: right;
-        }
-
         @media print {
             @page {
-                margin: 0;
-                size: 75mm 
-    ';
-    ?>
-    <?php 
-    $style .= 
-        ! empty($_COOKIE['innerHeight'])
-            ? $_COOKIE['innerHeight'] .'mm; }'
-            : '}';
-    ?>
-    <?php
-    $style .= '
-            html, body {
-                width: 70mm;
+                size: auto; /* Sesuaikan dengan ukuran lebar kertas printer Anda */
+                margin: 0p;
+            }
+            body {
+                size: 58mm;
+                font-family: "consolas", sans-serif;
+                margin: 0.1cm; /* Jarak 1cm pada kanan dan kiri */
+                padding: 0;
+                font-size: 10pt;
+            }
+            .text-center {
+                text-align: center;
+            }
+            .text-right {
+                text-align: right;
             }
             .btn-print {
                 display: none;
             }
         }
     </style>
-    ';
-    ?>
-
-    {!! $style !!}
 </head>
-<body onload="window.print()">
-    <button class="btn-print" style="position: absolute; right: 1rem; top: rem;" onclick="window.print()">Print</button>
+<body>
+    <button class="btn-print" style="position: absolute; right: 1rem; top: 1rem;" onclick="window.print()">Print</button>
     <div class="text-center">
         <h3 style="margin-bottom: 5px;">{{ strtoupper($setting->nama_perusahaan) }}</h3>
         <p>{{ strtoupper($setting->alamat) }}</p>
@@ -65,7 +42,7 @@
         <p style="float: left;">{{ date('d-m-Y') }}</p>
         <p style="float: right">{{ strtoupper(auth()->user()->name) }}</p>
     </div>
-    <div class="clear-both" style="clear: both;"></div>
+    <div style="clear: both;"></div>
     <p>No: {{ tambah_nol_didepan($penjualan->id_penjualan, 10) }}</p>
     <p class="text-center">===================================</p>
     
@@ -73,40 +50,37 @@
     <table width="100%" style="border: 0;">
         @foreach ($detail as $item)
         <tr>
-            <tr>
-                <td colspan="3">
-                    <?php
-                    $nama_produk = $item->produk->nama_produk;
-                    $tanggal_dibuat = $item->created_at; // Sesuaikan dengan atribut yang sesuai
-                    $paket_1_jam_anak = "Paket 1 Jam Anak";
-            
-                    if ($nama_produk == $paket_1_jam_anak) {
-                        // Tampilkan nama produk
-                        echo $nama_produk . "<br>";
-            
-                        // Tampilkan jam dibuatnya
-                        echo "Chekin: " . date('H:i', strtotime($tanggal_dibuat)) . "<br>";
-            
-                        // Lakukan penjumlahan 60 menit dari jam dibuatnya
-                        $tanggal_dibuat_plus_60_menit = strtotime('+60 minutes', strtotime($tanggal_dibuat));
-                        $jam_ditambah_60 = date('H:i', $tanggal_dibuat_plus_60_menit);
-            
-                        echo "Check Out: " . $jam_ditambah_60;
-                    } else {
-                        // Jika nama produk bukan "Paket 1 Jam Anak", tampilkan hanya nama produk
-                        echo $nama_produk;
-                    }
-                    ?>
-                </td>
-            </tr>
-            
+            <td colspan="3">
+                <?php
+                $nama_produk = $item->produk->nama_produk;
+                $tanggal_dibuat = $item->created_at; // Sesuaikan dengan atribut yang sesuai
+                $paket_1_jam_anak = "Paket 1 Jam Anak";
+        
+                if ($nama_produk == $paket_1_jam_anak) {
+                    // Tampilkan nama produk
+                    echo $nama_produk . "<br>";
+        
+                    // Tampilkan jam dibuatnya
+                    echo "Chekin: " . date('H:i', strtotime($tanggal_dibuat)) . "<br>";
+        
+                    // Lakukan penjumlahan 60 menit dari jam dibuatnya
+                    $tanggal_dibuat_plus_60_menit = strtotime('+60 minutes', strtotime($tanggal_dibuat));
+                    $jam_ditambah_60 = date('H:i', $tanggal_dibuat_plus_60_menit);
+        
+                    echo "Check Out: " . $jam_ditambah_60;
+                } else {
+                    // Jika nama produk bukan "Paket 1 Jam Anak", tampilkan hanya nama produk
+                    echo $nama_produk;
+                }
+                ?>
+            </td>
         </tr>
         
-            <tr>
-                <td>{{ $item->jumlah }} x {{ format_uang($item->harga_jual) }}</td>
-                <td></td>
-                <td class="text-right">{{ format_uang($item->jumlah * $item->harga_jual) }}</td>
-            </tr>
+        <tr>
+            <td>{{ $item->jumlah }} x {{ format_uang($item->harga_jual) }}</td>
+            <td></td>
+            <td class="text-right">{{ format_uang($item->jumlah * $item->harga_jual) }}</td>
+        </tr>
         @endforeach
     </table>
     <p class="text-center">-----------------------------------</p>
@@ -140,17 +114,14 @@
 
     <p class="text-center">===================================</p>
     <p class="text-center">-- TERIMA KASIH --</p>
-
-    <script>
-        let body = document.body;
-        let html = document.documentElement;
-        let height = Math.max(
-                body.scrollHeight, body.offsetHeight,
-                html.clientHeight, html.scrollHeight, html.offsetHeight
-            );
-
-        document.cookie = "innerHeight=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "innerHeight="+ ((height + 50) * 0.264583);
-    </script>
 </body>
+<script>
+    // Fungsi untuk mencetak konten ke printer termal
+    function printData() {
+        window.print();
+    }
+    // Panggil fungsi printData saat halaman dimuat
+    window.onload = printData;
+</script>
+
 </html>
