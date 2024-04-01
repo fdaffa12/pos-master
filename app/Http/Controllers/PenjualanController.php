f2493ef7b76d420132424e0a39ef82f67926a03d
fdaffa12 <faizdaffa17@gmail.com>
2024-04-01 14:24:01 +0700

    update

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -6,6 +6,7 @@ use App\Models\Penjualan;
 use App\Models\PenjualanDetail;
 use App\Models\Produk;
 use App\Models\Setting;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use PDF;
 use Illuminate\Support\Facades\Auth;
@@ -51,7 +52,32 @@ class PenjualanController extends Controller
         // Hitung total pendapatan (total bayar)
         $totalPendapatan = $penjualan->sum('bayar');
 
-        return view('penjualan.index', compact('tanggalAwal', 'tanggalAkhir', 'penjualan', 'totalPendapatan'));
+        $query = PenjualanDetail::select('produk.id_produk', 'produk.nama_produk', DB::raw('COUNT(*) as jumlah_penjualan'), DB::raw('SUM(subtotal) as total'))
+                    ->join('produk', 'penjualan_detail.id_produk', '=', 'produk.id_produk')
+                    ->whereBetween('penjualan_detail.created_at', [
+                        $tanggalAwal . ' 00:00:00',
+                        $tanggalAkhir . ' 23:59:59'
+                    ])
+                    ->groupBy('produk.id_produk', 'produk.nama_produk');
+
+        $penjualanPerProduk = $query->get();
+
+        $query = PenjualanDetail::select('produk.id_kategori', 'kategori.nama_kategori', DB::raw('COUNT(*) as jumlah_penjualan'), DB::raw('SUM(subtotal) as total'))
+            ->join('produk', 'penjualan_detail.id_produk', '=', 'produk.id_produk')
+            ->join('kategori', 'produk.id_kategori', '=', 'kategori.id_kategori')
+            ->whereBetween('penjualan_detail.created_at', [
+                $tanggalAwal . ' 00:00:00',
+                $tanggalAkhir . ' 23:59:59'
+            ])
+            ->groupBy('produk.id_kategori', 'kategori.nama_kategori');
+
+        $penjualanPerKategori = $query->get();
+
+
+
+
+
+        return view('penjualan.index', compact('tanggalAwal', 'penjualanPerKategori', 'penjualanPerProduk', 'tanggalAkhir', 'penjualan', 'totalPendapatan'));
     }
 
 
